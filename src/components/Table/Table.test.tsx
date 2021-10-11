4d7572a233b6177619d31db1cd0fcc72b9d6395b
Felix Schaffernicht <f.schaffernicht@syzygy.de>
2021-10-11 14:59:03 +0200

    Add basic table with tests

---
+++ src/components/Table/Table.test.tsx
@@ -0,0 +1,93 @@
+import { render, screen } from '@testing-library/react'
+import Table from './'
+
+describe('Should render', () => {
+  test('table', () => {
+    const config = [
+      {
+        field: 'a',
+        title: 'render a',
+        cellRenderer: () => {},
+      },
+      {
+        field: 'b',
+        title: 'render b',
+        cellRenderer: () => {},
+      },
+      {
+        field: 'c',
+        title: 'render c',
+        cellRenderer: () => {},
+      },
+    ]
+
+    const data = [
+      {
+        c: 'cell c1',
+        a: 'cell a1',
+        d: 'cell d1',
+        b: 'cell b1',
+      },
+      {
+        a: 'cell a2',
+        b: 'cell b2',
+        c: 'cell c2',
+        d: 'cell d2',
+      },
+      {
+        a: 'cell a3',
+        b: 'cell b3',
+        c: 'cell c3',
+        d: 'cell d3',
+      },
+    ]
+
+    const { container } = render(<Table configuration={config} data={data} />)
+    expect(container.firstChild).toBeInTheDocument()
+  })
+  test('table with data "cell a1"', () => {
+    const config = [
+      {
+        field: 'a',
+        title: 'render a',
+        cellRenderer: () => {},
+      },
+      {
+        field: 'b',
+        title: 'render b',
+        cellRenderer: () => {},
+      },
+      {
+        field: 'c',
+        title: 'render c',
+        cellRenderer: () => {},
+      },
+    ]
+
+    const data = [
+      {
+        a: 'cell a1',
+        b: 'cell b1',
+        c: 'cell c1',
+        d: 'cell d1',
+      },
+      {
+        a: 'cell a2',
+        b: 'cell b2',
+        c: 'cell c2',
+        d: 'cell d2',
+      },
+      {
+        a: 'cell a3',
+        b: 'cell b3',
+        c: 'cell c3',
+        d: 'cell d3',
+      },
+    ]
+
+    render(<Table configuration={config} data={data} />)
+
+    const customText = screen.getByText('cell a1')
+    expect(customText).toBeInTheDocument()
+  })
+})
---
+++ src/components/Table/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+// What the table needs
+// sorting
+// custom column styling
+// custom group heading
+// custom cell rendering
+
+interface Configuration {
+  field: string
+  size?: number | string
+  title?: string
+  cellRenderer?: (value?: string | number) => void
+}
+
+interface Data {
+  [key: string]: any
+}
+
+interface TableProps {
+  configuration: Configuration[]
+  data: Data[]
+}
+
+export default function Table(props: TableProps) {
+  const { configuration, data } = props
+  return (
+    <div>
+      {configuration.map((column, columnIndex) => {
+        return (
+          <div key={columnIndex}>
+            column {column.field}
+            <div>
+              {data.map((row, rowIndex) => {
+                if (row[column.field]) {
+                  return <div key={rowIndex}>{row[column.field]}</div>
+                }
+                return null
+              })}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
